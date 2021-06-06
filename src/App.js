469e74bb9a4cde478a3433457fbb3e94b83703b7
HammadKhaan <hammadkhan.1199@gmail.com>
2021-06-06 18:44:00 +0500

    Sign Up Form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import TeacherProfile from "./Screens/TeacherProfile"
 import {AuthContextProvider} from "./Contexts/AdminAuthContexts"
 import ResetPassword from "./Screens/ResetPassword"
 import ApplyForJob from "./Screens/ApplyForJob"
+import StudentSignUp from "./Screens/StudentSignUp"
+import StudentLogin from "./Screens/StudentLogin"
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
       <Route path="/teacher" component={TeacherProfile} />   
       <Route path="/resetpassword"  component={ResetPassword} />   
       <Route path="/careers" component={ApplyForJob}/> 
+      <Route path="/login" component={StudentLogin}/> 
+      <Route path="/signup" component={StudentSignUp}/> 
       
       <Route render={()=><h1>Not Found</h1>}></Route>    
       {/* Student Route  */}
