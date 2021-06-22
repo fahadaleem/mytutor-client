90dad49629bef4ef607536480593936bd7f58fbf
fahadaleem <faleem396@gmail.com>
2021-06-22 14:25:11 +0500

    Routes seprated from app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import "./App.css";
-import React, {useEffect, useState} from "react";
-import {Route, Switch} from "react-router-dom";
-import Admin from "./Screens/Admin";
-import LandingPage from "./Screens/LandingPage";
-import TeacherProfile from "./Screens/TeacherProfile";
-import ResetPassword from "./Screens/ResetPassword";
-import ApplyForJob from "./Screens/ApplyForJob";
-import StudentSignUp from "./Screens/StudentSignUp";
-import StudentLogin from "./Screens/StudentLogin";
+import React, { useEffect, useState } from "react";
+import AppRoutes from "./Routes";
 import CombineContext from "./Contexts/CombineContext";
-import StudentDashboard from "./Screens/StudentDashboard";
-import Page404 from "./Screens/Page404";
-import Error from "./Screens/NoInternetError"
+
+import Error from "./Screens/NoInternetError";
 
 function App() {
   const [isOnline, setIsOnline] = useState(window.navigator.onLine);
@@ -42,19 +34,7 @@ function App() {
     <>
       {isOnline ? (
         <CombineContext>
-          <Switch>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/admin" component={Admin} />
-            <Route path="/teacher" component={TeacherProfile} />
-            <Route path="/resetpassword" component={ResetPassword} />
-            <Route path="/careers" component={ApplyForJob} />
-            <Route path="/login" component={StudentLogin} />
-            <Route path="/signup" component={StudentSignUp} />
-            <Route path="/student" component={StudentDashboard} />
-
-            <Route component={Page404}></Route>
-            {/* Student Route  */}
-          </Switch>
+          <AppRoutes />
         </CombineContext>
       ) : (
         <Error />
