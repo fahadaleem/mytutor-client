cbc60bbf1a94977191f58b991f04fe113e5e0e27
fahadaleem <faleem396@gmail.com>
2021-07-03 05:47:46 +0500

    Filter Page Created

--- src/Screens/ViewAllCourses.js
+++ src/Screens/ViewAllCourses.js
@@ -1,34 +1,42 @@
-import React from "react"
-import courseCategories from "../categories.json"
-import { CourseContext } from "../Contexts/CourseContext"
-import CourseCard from "../Components/Courses/ViewAllCourses/Cards"
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import SimpleBackdrop from "../Components/Utilities/BackdropLoader"
+import React from "react";
+import courseCategories from "../categories.json";
+import { CourseContext } from "../Contexts/CourseContext";
+import CourseCard from "../Components/Courses/ViewAllCourses/Cards";
+import {Container, Grid} from "@material-ui/core";
+import SimpleBackdrop from "../Components/Utilities/BackdropLoader";
+import Typography from '@material-ui/core/Typography'
 
-const ViewAllCourses = ()=>{
-    console.log(courseCategories)
+const ViewAllCourses = () => {
+  console.log(courseCategories);
 
-    const {handleGetAllCourses, allCourses, loading} = React.useContext(CourseContext)
+  const { handleGetAllCourses, allCourses, loading } =
+    React.useContext(CourseContext);
 
-    React.useEffect(()=>{
-        handleGetAllCourses()
-    }, [])
+  React.useEffect(() => {
+    handleGetAllCourses();
+  }, []);
 
-    return(
-        <Container maxWidth="lg">
-            {loading&&<SimpleBackdrop />}
-          <Grid container spacing={3}>
-            {allCourses.map((elem)=>{
-                return (
-                    <Grid item lg={4}>
-                        <CourseCard category={elem.category} name={elem.name} id={elem.id} title={elem.title} description={elem.description}/>
-                    </Grid>
-                )
-            })}
-          </Grid>
-        </Container>
-    )
-}
+  return (
+    <Container maxWidth="lg">
+      {loading && <SimpleBackdrop />}
+      <Typography variant="h2" color="initial" align="center" style={{margin:"0 0 15px 0"}}>All Courses</Typography>
+      <Grid container spacing={3}>
+        {allCourses.map((elem) => {
+          return (
+            <Grid item lg={4}>
+              <CourseCard
+                category={elem.category}
+                name={elem.name}
+                id={elem.id}
+                title={elem.title}
+                description={elem.description}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+};
 
-export default ViewAllCourses
+export default ViewAllCourses;
