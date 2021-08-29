2360da704285f4cf04ae6a419ed7da826fb9e8ee
HammadKhaan <hammadkhan.1199@gmail.com>
2021-08-30 00:15:33 +0500

    Landing Page

---
+++ src/Components/LandingPage/Cards.js
@@ -0,0 +1,214 @@
+import React from 'react';
+import { makeStyles, Grid } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Image3 from "../../Assests/admin.png"
+import Image2 from "../../Assests/teacher.jpg"
+import Image1 from "../../Assests/student.png"
+
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+        height: "320px",
+        width: "350px",
+        marginLeft: "50px",
+    },
+    cards: {
+        display: "inline !important",
+    },
+});
+
+export default function Cards() {
+    const classes = useStyles();
+
+    return (
+        <Grid container style={{marginTop: "50px", marginBottom: "30px"}}>
+            <Grid item lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt="Contemplative Reptile"
+                            height="140"
+                            image={Image1}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
+                                STUDENT
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" style={{textAlign:"center"}}>
+                                Get Access to 100+ courses from our professional and highly trained teachers and start learning today!
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px",paddingBottom:"40px",}}>
+                            Login
+                        </Button>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px",paddingBottom:"40px",}}>
+                            Sign Up
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+
+            <Grid item lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt="Contemplative Reptile"
+                            height="140"
+                            image={Image2}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
+                                TEACHER
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" style={{textAlign:"center"}}>
+                                Prepare your courses, get verified from us and start teaching now!
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px",paddingTop:"22px",}}>
+                            Login
+                        </Button>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px",paddingTop:"22px",}}>
+                            Sign Up
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+
+            <Grid item lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt="Contemplative Reptile"
+                            height="140"
+                            image={Image3}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
+                                ADMIN
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" style={{textAlign:"center"}}>
+                                For Administration use only!
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px",paddingTop:"40px",}}>
+                            Login
+                        </Button>
+                        <Button size="small" color="primary" style={{paddingLeft:"80px", paddingTop:"40px",}}>
+                            Sign Up
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </Grid>
+
+
+
+        // <div className={classes.cards}>
+
+        // <Card className={classes.root}>
+        //   <CardActionArea>
+        //     <CardMedia
+        //       component="img"
+        //       alt="Contemplative Reptile"
+        //       height="140"
+        //       image="../Assets/admin.png"
+        //       title="Contemplative Reptile"
+        //     />
+        //     <CardContent>
+        //       <Typography gutterBottom variant="h5" component="h2">
+        //         STUDENT
+        //       </Typography>
+        //       <Typography variant="body2" color="textSecondary" component="p">
+        //         Get Access to 100+ courses from our professional and highly trained teachers and start learning today!
+        //       </Typography>
+        //     </CardContent>
+        //   </CardActionArea>
+        //   <CardActions>
+        //     <Button size="small" color="primary">
+        //       Login
+        //     </Button>
+        //     <Button size="small" color="primary">
+        //       Sign Up
+        //     </Button>
+        //   </CardActions>
+        // </Card>
+
+        // <Card className={classes.root}>
+        //   <CardActionArea>
+        //     <CardMedia
+        //       component="img"
+        //       alt="Contemplative Reptile"
+        //       height="140"
+        //       image="../Assets/admin.png"
+        //       title="Contemplative Reptile"
+        //     />
+        //     <CardContent>
+        //       <Typography gutterBottom variant="h5" component="h2">
+        //        TEACHER
+        //       </Typography>
+        //       <Typography variant="body2" color="textSecondary" component="p">
+        //         Prepare your courses, get verified from us and start teaching now! 
+        //       </Typography>
+        //     </CardContent>
+        //   </CardActionArea>
+        //   <CardActions>
+        //     <Button size="small" color="primary">
+        //       Login
+        //     </Button>
+        //     <Button size="small" color="primary">
+        //       Sign Up
+        //     </Button>
+        //   </CardActions>
+        // </Card>
+
+
+        // <Card className={classes.root}>
+        //   <CardActionArea>
+        //     <CardMedia
+        //       component="img"
+        //       alt="Contemplative Reptile"
+        //       height="140"
+        //       image="../Assets/admin.png"
+        //       title="Contemplative Reptile"
+        //     />
+        //     <CardContent>
+        //       <Typography gutterBottom variant="h5" component="h2">
+        //         ADMIN
+        //       </Typography>
+        //       <Typography variant="body2" color="textSecondary" component="p">
+        //         For Administration use only! 
+        //       </Typography>
+        //     </CardContent>
+        //   </CardActionArea>
+        //   <CardActions>
+        //     <Button size="small" color="primary">
+        //       Login
+        //     </Button>
+        //     <Button size="small" color="primary">
+        //       Sign Up
+        //     </Button>
+        //   </CardActions>
+        // </Card>
+
+        // </div>
+    );
+}
--- src/Screens/LandingPage.js
+++ src/Screens/LandingPage.js
@@ -1,12 +1,71 @@
 import React from "react"
 import Navbar from "../Components/Navbar/MainNav"
+import Avatar from '@material-ui/core/Avatar'
+import CardHeader from '@material-ui/core/CardHeader'
+import IconButton from '@material-ui/core/IconButton'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { Grid, makeStyles, Grow, Typography, Button } from '@material-ui/core'
+import Image from "../Assests/header1.jpg"
+import Cards from "../Components/LandingPage/Cards"
 
-const LandingPage = ()=>{
+const useStyles = makeStyles(theme => ({
+    header: {
+        height: "400px",
+        background: "linear-gradient(90deg,#94bbe9,#4466df)",
+
+
+    },
+    image: {
+        //  background: "linear-gradient(90deg,#94bbe9,#4466df)",
+        paddingTop: "40px",
+        paddingLeft: "100px",
+
+
+    }
+}))
+
+const LandingPage = () => {
+    const classes = useStyles();
+
+
     return (
 
         <div>
             <Navbar pageName="home" />
-            <h1>Landing here</h1>
+            <div className={classes.header}>
+
+                <Grid container>
+
+
+                    <Grid item lg={6}>  <Typography variant="h4" color="initial" style={{ paddingTop: "40px", paddingLeft: "30px" }}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita esse adipisci culpa magnam tempora maxime dolor molestias
+                        alias minus maiores.
+
+                    </Typography>
+                        <Button variant="contained" color="primary" style={{ marginTop: "80px", marginLeft: "30px", width: "150px", height: "50px" }}>
+                            Get Started
+                        </Button>
+                        
+                    </Grid>
+
+
+
+                    <div className={classes.image}>
+                        <Grid item lg={6}>  <img src={Image} alt="" style={{ width: "500px", height: "320px" }} /> </Grid>
+
+                    </div>
+
+                </Grid>
+
+            </div>
+            {/* <h1>landing here</h1> */}
+
+            <Cards />
+
+
+
+
         </div>
     )
 }
